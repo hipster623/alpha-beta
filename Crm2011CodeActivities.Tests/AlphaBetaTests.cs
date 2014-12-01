975d691196647e7d6434884617fccab1c908ce27
tj <hipster623@users.noreply.github.com>
2014-12-01 23:43:39 +0100

    code to find complete number, unit tests for the nice to haves

--- Crm2011CodeActivities.Tests/AlphaBetaTests.cs
+++ Crm2011CodeActivities.Tests/AlphaBetaTests.cs
@@ -18,6 +18,27 @@ namespace Crm2011CodeActivities.Tests
                         "    <td>Row 2 Cell 2 20068346363</td>" + //TODO: create real test SSN
                         "  </tr>" +
                         "</table>";
+        string _sampleText_level2 = "<table border=\"1\">" +
+                        " <tr>" +
+                        "    <td>Row 1 Cell 1</td>" +
+                        "    <td>Row 1 Cell 2</td>" +
+                        "  </tr>" +
+                        "  <tr>" +
+                        "    <td>Row 2 Cell 1</td>" +
+                        "    <td>Row 2 Cell 2 200683 46363</td>" + //TODO: create real test SSN
+                        "  </tr>" +
+                        "</table>";
+        string _sampleText_level3 = "<table border=\"1\">" +
+                                " <tr>" +
+                                "    <td>Row 1 Cell 1</td>" +
+                                "    <td>Row 1 Cell 2</td>" +
+                                "  </tr>" +
+                                "  <tr>" +
+                                "    <td>Row 2 Cell 1</td>" +
+                                "    <td>Row 2 Cell 2 20.06.83 46363</td>" + //TODO: create real test SSN
+                                "  </tr>" +
+                                "</table>";
+
 
         [TestMethod]
         public void CanReadArguments()
@@ -32,8 +53,8 @@ namespace Crm2011CodeActivities.Tests
             var output = WorkflowInvoker.Invoke(activity, input);
             
             //assert
-            Assert.AreEqual(input["TextToParse"], output["Found"]);
-            Assert.AreEqual(0, output["Prob"]);
+            Assert.IsNotNull(output["Found"]);
+            Assert.IsNotNull(output["Prob"]);
         }
 
         /// <summary>
@@ -54,6 +75,45 @@ namespace Crm2011CodeActivities.Tests
             
             //assert
             Assert.AreEqual("20068346363", output["Found"]);
+        }
+
+        [TestMethod]
+        public void ShouldFindSsnInText_WithSpacing()
+        {
+            //arrange
+            var activity = new AlphaBeta();
+            var input = new Dictionary<string, object>() {
+                {"TextToParse", _sampleText_level2 }
+            };
+
+            //act
+            var output = WorkflowInvoker.Invoke(activity, input);
+
+            //assert
+            Assert.AreEqual("20068346363", output["Found"]);
+        }
+
+        [TestMethod]
+        public void ShouldFindSsnInText_WithDotsAndSpacing()
+        {
+            //arrange
+            var activity = new AlphaBeta();
+            var input = new Dictionary<string, object>() {
+                {"TextToParse", _sampleText_level3 }
+            };
+
+            //act
+            var output = WorkflowInvoker.Invoke(activity, input);
+
+            //assert
+            Assert.AreEqual("20068346363", output["Found"]);
+        }
+
+        [TestMethod]
+        public void LuhnTestShouldFindValidSsn() {
+            var invalidSsn = "12345612345";
+            var result = new AlphaBeta().DoLuhnMod10(invalidSsn);
+            Assert.AreEqual(false, result);
         }
     }
 }
--- Crm2011CodeActivities/AlphaBeta.cs
+++ Crm2011CodeActivities/AlphaBeta.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Workflow;
+using System.Text.RegularExpressions;
 
 namespace Crm2011CodeActivities
 {
@@ -19,8 +20,31 @@ namespace Crm2011CodeActivities
 
         protected override void Execute(CodeActivityContext context)
         {
-            Found.Set(context, TextToParse.Get(context));
-            Prob.Set(context, 0);
+            //PSEUDO
+            //get text
+            var text = TextToParse.Get(context);
+            //find all numbers in text
+            var numbers = FindAllNumbers(text);
+
+            //discard anyone not 11 digits, do Luhn mod 10 on them, put hits in candidate array
+            var candidatesCorrectLength = numbers.Where(x => (x.Length == 11)).ToList();
+            var candidates = candidatesCorrectLength.Where(y => DoLuhnMod10(y)).ToList();
+
+            Found.Set(context, candidates.FirstOrDefault());
+            Prob.Set(context, candidates.Count);
+        }
+
+        public bool DoLuhnMod10(string y)
+        {
+            return true;
+        }
+
+        private List<string> FindAllNumbers(string text)
+        {            
+            // Split on one or more non-digit characters.
+            string[] numbers = Regex.Split(text, @"\D+");
+            
+            return new List<string>(numbers);
         }
     }
 }
