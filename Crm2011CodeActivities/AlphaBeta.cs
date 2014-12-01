10045f7e2d9fb7016d901ee3e9a2c384b8642742
tj <tomazio@outlook.com>
2014-12-01 22:11:43 +0100

    changed for crm 2011 compatible

---
+++ Crm2011CodeActivities/AlphaBeta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace Crm2011CodeActivities
+{
+    public class AlphaBeta : CodeActivity
+    {
+        protected override void Execute(CodeActivityContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
